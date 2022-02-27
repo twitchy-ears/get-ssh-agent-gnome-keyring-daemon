650d97f6dad90bc20b8bc9f29ec31fed76567a7d
twitchy-ears <45134107+twitchy-ears@users.noreply.github.com>
2022-02-27 20:56:39 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # get-ssh-agent-gnome-keyring-daemon
-Fetch SSH_AGENT_SOCK and SSH_AGENT_PID in use by gnome-keyring-daemon
+
+On Ubuntu 21.10 my Emacs running keychain-refresh-environment doesn't interact well with the gnome-keyring-daemon so it starts a fresh agent and doesn't get the right ```SSH_AGENT_SOCK``` and ```SSH_AUTH_SOCK```.
+
+This very fragile little bit of emacs code looks for a socket in the place my gnome-keyring-daemon stores it, and if it exists calls pgrep to try and get the PID of the agent associated with it.
+
+All you really need us something like this:
+
+```
+(use-package get-ssh-agent-gnome-keyring-daemon
+ :config
+ (get-ssh-agent-gnome-keyring-daemon))
+```
+
+It will attempts to look for a socket in ```/run/user/<uid>/keyring/.ssh``` then get the PID for the ssh-agent associated.
+
+This was written because on Ubuntu 21.10 my keychain-refresh-environment doesn't work, so now for my portable config I have:
+
+```
+(use-package get-ssh-agent-gnome-keyring-daemon
+ :config
+ (get-ssh-agent-gnome-keyring-daemon))
+
+(use-package keychain-environment
+ :ensure t
+ :config
+ (if (not get-ssh-agent-gnome-keyring-daemon)
+     (keychain-refresh-environment)))
+```
+
+Which means if it gets it from the gnome-keyring-daemon it doesn't
+try keychain-refresh-environment.
+
+If your socket is in another location you can call:
+
+```
+(get-ssh-agent-gnome-keyring-daemon "/path/to/your/socket")
+```
+
+Just keep in mind this is going to be put into a pgrep shell command and I can't guarentee this is totally secure.  You have been warned.
+
+
+
+
---
+++ get-ssh-agent-gnome-keyring-daemon.el
@@ -0,0 +1,106 @@
+;;; get-ssh-agent-gnome-keyring-daemon.el --- Gets SSH_AUTH_SOCK and SSH_AUTH_PID for an SSH agent running under Gnome Keyring Daemon -*- lexical-binding: t -*-
+
+;; Copyright 2021 - Twitchy Ears
+
+;; Author: Twitchy Ears https://github.com/twitchy-ears/
+;; URL: https://github.com/twitchy-ears/get-ssh-agent-gnome-keyring-daemon
+;; Version: 0.1
+;; Package-Requires ((emacs "26.1"))
+;; Keywords: ssh agent
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+
+;; This is not a real solid package, this is just me breaking out my
+;; nanowrimo-setup it does include accurate to wc(1) word counting, a
+;; split outline/editing view and plain text output (part of the
+;; wordcounting)
+
+;;; History
+;;
+;; 2022-02-27 - initial version
+
+;;; Commentary:
+
+;; All you really need us something like this:
+;; (use-package get-ssh-agent-gnome-keyring-daemon
+;;  :config
+;;  (get-ssh-agent-gnome-keyring-daemon))
+;;
+;; It will attempts to look for a socket in
+;; /run/user/<uid>/keyring/.ssh then get the PID for the ssh-agent
+;; associated.
+;;
+;; This was written because on Ubuntu 21.10 my
+;; keychain-refresh-environment doesn't work, so now for my portable
+;; config I have:
+;;
+;; (use-package get-ssh-agent-gnome-keyring-daemon
+;;  :config
+;;  (get-ssh-agent-gnome-keyring-daemon))
+;; 
+;; (use-package keychain-environment
+;;  :ensure t
+;;  :config
+;;  (if (not get-ssh-agent-gnome-keyring-daemon)
+;;      (keychain-refresh-environment)))
+;;
+;; Which means if it gets it from the gnome-keyring-daemon it doesn't
+;; try keychain-refresh-environment.
+
+;;; Code:
+(defvar get-ssh-agent-gnome-keyring-daemon nil
+  "Defaults to nil, if get-ssh-agent-gnome-keyring-daemon is run and successfully locates an ssh-agent PID it will be set to t")
+
+(defun get-ssh-agent-gnome-keyring-daemon (&optional SOCKETNAME)
+  "Looks for a socket at /run/user/<uid>/keyring/.ssh and if it finds one it uses pgrep to try and find the ssh-agent command associated with it and get its PID, if it has both it sets the SSH_AGENT_SOCK and SSH_AGENT_PID environment variables and sets 'get-ssh-agent-gnome-keyring-daemon' to t, otherwise this variable is set to nil.
+
+Takes an optional argument of SOCKETNAME and if set will use that instead of trying to generate one.
+
+WARNING: there is a non-zero chance that this string could be used to try and exploit your shell via a shell escape."
+  (let* ((socktmp (if SOCKETNAME
+                      SOCKETNAME
+                    (concat "/run/user/"
+                            (format "%i" (user-uid))
+                            "/keyringBROKEN/.ssh")))
+         (agentpid nil)
+         (pgrep-command (format "pgrep -nfi 'ssh-agent -D -a %s'" socktmp))
+         (kill-buffer-query-functions nil))
+    (progn
+      
+      ;; If the socket exists get the ssh-agent PID associated with it
+      (if (file-exists-p socktmp)
+
+          ;; Try and use pgrep to get the PID for the agent
+          (save-excursion
+            (with-temp-buffer
+              (let ((tmpbuffer (buffer-name)))
+                (shell-command pgrep-command tmpbuffer)
+                (switch-to-buffer tmpbuffer)
+                (setq agentpid (buffer-substring-no-properties (point-min) (point-max)))
+
+                ;; Should regex the PID but doesn't.
+                (if (not (eql agentpid nil))
+                    (progn
+                      (setenv "SSH_AGENT_SOCK" socktmp)
+                      (setenv "SSH_AGENT_PID" agentpid)
+                      (setq get-ssh-agent-gnome-keyring-daemon t))))))
+        
+        ;; Otherwise indicate failure
+        (setq get-ssh-agent-gnome-keyring-daemon nil)))))
+
+(provide 'get-ssh-agent-gnome-keyring-daemon)
